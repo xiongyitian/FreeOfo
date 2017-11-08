2aeeff0a75cd8a77c00019c0b66d536119c1bed8
xiongyitian <xiongyitian@yunba.io>
2017-11-08 18:07:13 +0800

    Initial Commit

---
+++ FreeOfoUITests/FreeOfoUITests.swift
@@ -0,0 +1,36 @@
+//
+//  FreeOfoUITests.swift
+//  FreeOfoUITests
+//
+//  Created by yunba on 2017/11/8.
+//  Copyright © 2017年 yunba. All rights reserved.
+//
+
+import XCTest
+
+class FreeOfoUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
